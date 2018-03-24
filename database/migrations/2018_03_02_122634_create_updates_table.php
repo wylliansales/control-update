efb527cdad8d1aac8cbd71208282f218f750fdd6
wylliansales <wylliansalles@gmail.com>
2018-03-24 13:55:15 -0300

    Iniciando ...........!

---
+++ database/migrations/2018_03_02_122634_create_updates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('updates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();           
+            $table->string('update_path');
+            $table->text('news')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            
+
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('updates');
+    }
+}
---
+++ database/migrations/2018_03_16_182621_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id')->unsigned(); 
+            $table->integer('product_id')->unsigned();           
+            $table->text('comments');
+            $table->boolean('status')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
